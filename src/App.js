cc7bc74099401127eed210d5c1f2cf689f7bbf14
coffeeandpouch <sameinsidler@me.com>
2020-10-16 13:48:29 -0500

    latest changes

--- src/App.js
+++ src/App.js
@@ -75,8 +75,7 @@ class App extends Component {
       );
     });
 
-    const summary = CartSummary;
-    Object.keys(this.state.selected).map((feature, idx) => {
+    const summary = Object.keys(this.state.selected).map((feature, idx) => {
       const featureHash = feature + "-" + idx;
       const selectedOption = this.state.selected[feature];
 
