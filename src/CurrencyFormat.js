d07f8317b591650f75348fd0501989dde8abbdfa
coffeeandpouch <sameinsidler@me.com>
2020-10-17 13:34:01 -0500

    latest changes

---
+++ src/CurrencyFormat.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+//This object will allow us to
+// easily convert numbers into US dollar values
+
+class CurrencyFormat extends component {
+render() {
+ return(
+
+const USCurrencyFormat = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+});
+
+}
