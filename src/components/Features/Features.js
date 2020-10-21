dbaa0e8a3961ac0afc09eaa1d6927be452f42cf2
coffeeandpouch <sameinsidler@me.com>
2020-10-20 21:29:21 -0500

    Breaks App down into 3 smaller components

---
+++ src/components/Features/Features.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+// Normalizes string as a slug - a string that is safe to use
+// in both URLs and html attributes
+import slugify from "slugify";
+
+export default function Features(props) {
+  const { USCurrencyFormat, selected, features } = props;
+
+  return Object.keys(features).map((feature, idx) => {
+    const featureHash = feature + "-" + idx;
+    const options = features[feature].map((item) => {
+      const itemHash = slugify(JSON.stringify(item));
+      return (
+        <div key={itemHash} className="feature__item">
+          <input
+            type="radio"
+            id={itemHash}
+            className="feature__option"
+            name={slugify(feature)}
+            checked={item.name === selected[feature].name}
+            onChange={(e) => props.updateFeature(feature, item)}
+          />
+          <label htmlFor={itemHash} className="feature__label">
+            {item.name} ({USCurrencyFormat.format(item.cost)})
+          </label>
+        </div>
+      );
+    });
+
+    return (
+      <fieldset className="feature" key={featureHash}>
+        <legend className="feature__name">
+          <h3>{feature}</h3>
+        </legend>
+        {options}
+      </fieldset>
+    );
+  });
+}
