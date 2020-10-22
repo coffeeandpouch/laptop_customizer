d1e99bf78c358b6272187cc2a5bd821f13899e38
coffeeandpouch <sameinsidler@me.com>
2020-10-22 15:23:16 -0500

    makes appropriate changes to keep working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from "react";
-import CartSummary from "CartSummary";
-import CartTotal from "CartTotal";
+import React, { Component } from "react";
 
-// Normalizes string as a slug - a string that is safe to use
-// in both URLs and html attributes
-import slugify from "slugify";
+import Total from "./components/Total/Total";
+import Summary from "./components/Summary/Summary";
+import Features from "./components/Features/Features";
 
 import "./App.css";
+
+// This object will allow us to
+// easily convert numbers into US dollar values
+const USCurrencyFormat = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+});
 
 class App extends Component {
   state = {
@@ -39,36 +44,34 @@ class App extends Component {
   };
 
   render() {
-    const features = Object.keys(this.props.features).map((feature, idx) => {
-      const featureHash = feature + "-" + idx;
-      const options = this.props.features[feature].map((item) => {
-        const itemHash = slugify(JSON.stringify(item));
-        return (
-          <div key={itemHash} className="feature__item">
-            <input
-              type="radio"
-              id={itemHash}
-              className="feature__option"
-              name={slugify(feature)}
-              checked={item.name === this.state.selected[feature].name}
-              onChange={(e) => this.updateFeature(feature, item)}
-            />
-            <label htmlFor={itemHash} className="feature__label">
-              {item.name} ({USCurrencyFormat.format(item.cost)})
-            </label>
-          </div>
-        );
-      });
+    const total = Object.keys(this.state.selected).reduce(
+      (acc, curr) => acc + this.state.selected[curr].cost,
+      0
+    );
 
-      return (
-        <fieldset className="feature" key={featureHash}>
-          <legend className="feature__name">
-            <h3>{feature}</h3>
-          </legend>
-          {options}
-        </fieldset>
-      );
-    });
+    return (
+      <div className="App">
+        <header>
+          <h1>ELF Computing | Laptops</h1>
+        </header>
+        <main>
+          <form className="main__form">
+            <h2>Customize your laptop</h2>
+            <Features
+              USCurrencyFormat={USCurrencyFormat}
+              updateFeature={this.updateFeature}
+              {...this.state}
+              {...this.props}
+            />
+          </form>
+          <section className="main__summary">
+            <h2>Your cart</h2>
+            <Summary USCurrencyFormat={USCurrencyFormat} {...this.state} />
+            <Total USCurrencyFormat={USCurrencyFormat} total={total} />
+          </section>
+        </main>
+      </div>
+    );
   }
 }
 
