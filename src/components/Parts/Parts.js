8f8060d20c4b4bbab8d589316b5ef299598c93dd
coffeeandpouch <sameinsidler@me.com>
2020-10-27 13:10:24 -0500

    7 of 7

--- src/components/Parts/Parts.js
+++ src/components/Parts/Parts.js
@@ -1,16 +1,26 @@
 import React from "react";
 
-export default function Parts(props) {
-  const options = features[feature].map((item) => {
-    const itemHash = slugify(JSON.stringify(item));
+// Normalizes string as a slug - a string that is safe to use
+// in both URLs and html attributes
+import slugify from "slugify";
 
-    return (
-      <fieldset className="feature" key={featureHash}>
-        <legend className="feature__name">
-          <h3>{feature}</h3>
-        </legend>
-        {options}
-      </fieldset>
-    );
-  });
+export default function Parts(props) {
+  const { USCurrencyFormat, selected, feature, item } = props;
+
+  const itemHash = slugify(JSON.stringify(item));
+  return (
+    <div key={itemHash} className="feature__item">
+      <input
+        type="radio"
+        id={itemHash}
+        className="feature__option"
+        name={slugify(feature)}
+        checked={item.name === selected[feature].name}
+        onChange={(e) => props.updateFeature(feature, item)}
+      />
+      <label htmlFor={itemHash} className="feature__label">
+        {item.name} ({USCurrencyFormat.format(item.cost)})
+      </label>
+    </div>
+  );
 }
--- src/components/Specs/Specs.js
+++ src/components/Specs/Specs.js
@@ -1,31 +1,14 @@
 import React from "react";
 
-// Normalizes string as a slug - a string that is safe to use
-// in both URLs and html attributes
-import slugify from "slugify";
+import Parts from "../Parts/Parts";
 
 export default function Specs(props) {
-  const { USCurrencyFormat, selected, features, feature, idx } = props;
+  const { features, feature, idx } = props;
 
   const featureHash = feature + "-" + idx;
-  const options = features[feature].map((item) => {
-    const itemHash = slugify(JSON.stringify(item));
-    return (
-      <div key={itemHash} className="feature__item">
-        <input
-          type="radio"
-          id={itemHash}
-          className="feature__option"
-          name={slugify(feature)}
-          checked={item.name === selected[feature].name}
-          onChange={(e) => props.updateFeature(feature, item)}
-        />
-        <label htmlFor={itemHash} className="feature__label">
-          {item.name} ({USCurrencyFormat.format(item.cost)})
-        </label>
-      </div>
-    );
-  });
+  const options = features[feature].map((item) => (
+    <Parts {...props} item={item} />
+  ));
 
   return (
     <fieldset className="feature" key={featureHash}>
